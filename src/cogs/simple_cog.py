05f07dedd6de509d104c7fd3d9484400a6a8c798
murilobnt <murilo.araujo.md@gmail.com>
2020-11-30 23:47:44 -0300

    Issue #2: First cog implementation

---
+++ src/cogs/simple_cog.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+class Misc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def say_hi(self, ctx):
+        await ctx.send("Hi!")
