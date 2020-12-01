84e7f03ab9b87f2caa59b0c168adbf392a1f9814
murilobnt <murilo.araujo.md@gmail.com>
2020-11-30 23:49:53 -0300

    Issue #2: Adds cog to bot

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 
+from src.cogs.simple_cog import SimpleCog
+
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
@@ -11,4 +13,5 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+bot.add_cog(SimpleCog(bot))
 bot.run(os.environ['DISCORD_BOT_KEY'])
--- src/cogs/simple_cog.py
+++ src/cogs/simple_cog.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class Misc(commands.Cog):
+class SimpleCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
