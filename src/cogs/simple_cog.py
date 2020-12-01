594518adc7857f08d174cdb26a7648e39411b3a7
murilobnt <murilo.araujo.md@gmail.com>
2020-11-30 23:53:00 -0300

    Issue #2: Adds say_datetime command to bot

--- src/cogs/simple_cog.py
+++ src/cogs/simple_cog.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+from datetime import datetime
+
 class SimpleCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -8,3 +10,7 @@ class SimpleCog(commands.Cog):
     @commands.command(pass_context=True)
     async def say_hi(self, ctx):
         await ctx.send("Hi!")
+
+    @commands.command(pass_context=True)
+    async def say_datetime(self, ctx):
+        await ctx.send(str(datetime.now()))
