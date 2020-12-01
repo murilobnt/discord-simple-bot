9480feba86bad03efac5556cfdc85c54f7baa4e6
murilobnt <murilo.araujo.md@gmail.com>
2020-11-30 23:40:48 -0300

    Issue #1: Discord bot online

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+import os
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+bot.run(os.environ['DISCORD_BOT_KEY'])
